abd0e8b954a1974596a3a1059eabcfdfe3b691c4
kkkirill <kkg.410@mail.ru>
2019-08-21 18:38:26 +0300

    insignificantly changed template, changed generating cookie

--- HTTPlayground/settings.py
+++ HTTPlayground/settings.py
@@ -4,18 +4,18 @@ from jinja2 import Environment, FileSystemLoader
 
 SERVERS_INFO = {
     'URL0': '127.0.0.1', 'PORT0': 8000,
-    'URL1': '0.0.0.0', 'PORT1': 8080  # 127.255.255.254
+    'URL1': '0.0.0.0', 'PORT1': 8080
 }
 
 SALT = "удав"
 
-IS_MAIN_SERVER = not int(argv[1])
+IS_MAIN_SERVER = argv[1] == '0'
 
-PORT = SERVERS_INFO.get(f'PORT{argv[1]}', SERVERS_INFO['PORT0'])
-URL = SERVERS_INFO.get(f'URL{argv[1]}', SERVERS_INFO['URL0'])
+PORT = SERVERS_INFO[f'PORT{argv[1]}']
+URL = SERVERS_INFO[f'URL{argv[1]}']
 
-ACCESSORY_PORT = SERVERS_INFO.get(f"PORT{1 - int(argv[1])}", "PORT1")
-ACCESSORY_URL = SERVERS_INFO.get(f"URL{1 - int(argv[1])}", "URL1")
+ACCESSORY_PORT = SERVERS_INFO[f"PORT{1 - int(argv[1])}"]
+ACCESSORY_URL = SERVERS_INFO[f"URL{1 - int(argv[1])}"]
 
 MAIN_URL_PREFIX = f'http://{URL}:{PORT}'
 ACCESSORY_URL_PREFIX = f'http://{ACCESSORY_URL}:{ACCESSORY_PORT}'
